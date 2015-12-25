e729f5977ba1042c04691ed41775baf16b6a4fad
lidanyang <1261727840@qq.com>
2015-12-25 17:55:34 +0800

    第三章

---
+++ kernel_ldd3.mm
@@ -0,0 +1,61 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1451031058025" ID="ID_332111700" MODIFIED="1451031112361" TEXT="&#x5b57;&#x7b26;&#x9a71;&#x52a8;">
+<node CREATED="1451031067532" FOLDED="true" ID="ID_802837188" MODIFIED="1451031694767" POSITION="right" TEXT="scull">
+<node CREATED="1451031236139" ID="ID_1193841615" MODIFIED="1451031325961" TEXT="scull0-3 &#x5168;&#x5c40;&#x6c38;&#x4e45;"/>
+<node CREATED="1451031309836" ID="ID_1540273098" MODIFIED="1451031419569" TEXT="scullpipe0-3&#x5148;&#x5165;&#x5148;&#x51fa; &#x963b;&#x585e;/&#x975e;&#x963b;&#x585e;&#x8bfb;&#x5199;"/>
+<node CREATED="1451031433405" ID="ID_1115595684" MODIFIED="1451031474830" TEXT="scullsingle scullpriv sculluid scullwuid &#x548c;scull0&#x76f8;&#x4f3c;">
+<node CREATED="1451031474831" ID="ID_1080713894" MODIFIED="1451031498138" TEXT="scullsingle&#x4e00;&#x6b21;&#x53ea;&#x5141;&#x8bb8;&#x4e00;&#x4e2a;&#x8fdb;&#x7a0b;&#x4f7f;&#x7528;"/>
+<node CREATED="1451031498972" ID="ID_1746917963" MODIFIED="1451031529181" TEXT="scullpriv&#x5bf9;&#x6bcf;&#x4e2a;&#x865a;&#x62df;&#x7ec8;&#x7aef;&#x662f;&#x79c1;&#x6709;"/>
+<node CREATED="1451031530284" ID="ID_1923953408" MODIFIED="1451031588650" TEXT="sculluid &#x548c;scullwuid&#x53ef;&#x4ee5;&#x591a;&#x6b21;&#x6253;&#x5f00; &#x4e00;&#x6b21;&#x53ea;&#x80fd;&#x662f;&#x4e00;&#x4e2a;&#x7528;&#x6237;">
+<node CREATED="1451031592748" ID="ID_1739487320" MODIFIED="1451031604809" TEXT="&#x524d;&#x8005;&#x8fd4;&#x56de;&#x8bbe;&#x5907;&#x5fd9;&#x7684;&#x9519;&#x8bef;"/>
+<node CREATED="1451031606188" ID="ID_1081733695" MODIFIED="1451031613433" TEXT="&#x540e;&#x8005;&#x5b9e;&#x73b0;&#x963b;&#x585e;&#x6253;&#x5f00;"/>
+</node>
+</node>
+<node CREATED="1451031441084" ID="ID_169189050" MODIFIED="1451031441084" TEXT=""/>
+</node>
+<node CREATED="1451031428332" ID="ID_1054063932" MODIFIED="1451031700714" POSITION="right" TEXT="&#x4e3b;&#x6b21;&#x7f16;&#x53f7;">
+<node CREATED="1451031706828" ID="ID_1726942250" MODIFIED="1451031753002" TEXT="&#x4e00;&#x4e2a;&#x4e3b;&#x7f16;&#x53f7;&#x4e00;&#x4e2a;&#x9a71;&#x52a8;"/>
+<node CREATED="1451031753436" ID="ID_495092805" MODIFIED="1451031794922" TEXT="&#x6b21;&#x7f16;&#x53f7;&#x51b3;&#x5b9a;&#x5f15;&#x7528;&#x54ea;&#x4e2a;&#x8bbe;&#x5907;"/>
+<node CREATED="1451031795420" ID="ID_1185163139" MODIFIED="1451031915549" TEXT="&#x8bbe;&#x5907;&#x7f16;&#x53f7;&#x5185;&#x90e8;&#x8868;&#x793a;">
+<node CREATED="1451031858284" ID="ID_1286686823" MODIFIED="1451031904383" TEXT="MAJOR(dev_t dev) MINOR(dev_t dev) MKDEV(int major,int minor)"/>
+<node CREATED="1451031915549" ID="ID_1951341559" MODIFIED="1451031947258" TEXT="dev_t 32&#x4f4d; 12&#x4e3b; 20&#x6b21; linux/kdev_t.h"/>
+</node>
+<node CREATED="1451031911916" ID="ID_871240374" MODIFIED="1451036374565" TEXT="&#x5206;&#x914d;&#x91ca;&#x653e;&#x8bbe;&#x5907;&#x7f16;&#x53f7;">
+<node CREATED="1451032240526" ID="ID_1474748445" MODIFIED="1451032264042" TEXT="linux/fs.h"/>
+<node CREATED="1451032109391" FOLDED="true" ID="ID_1785821731" MODIFIED="1451032362319" TEXT="int register_chrdev_region(dev_t first,unsigned int count,char *name);">
+<node CREATED="1451032146719" ID="ID_101550217" MODIFIED="1451032189978" TEXT="first &#x8d77;&#x59cb;&#x8bbe;&#x5907;&#x7f16;&#x53f7;"/>
+<node CREATED="1451032190461" ID="ID_671789011" MODIFIED="1451032217673" TEXT="count&#x662f;&#x8fde;&#x7eed;&#x7f16;&#x53f7;&#x7684;&#x603b;&#x6570; &#x592a;&#x5927;&#x6ea2;&#x51fa;&#x5230;&#x4e0b;&#x4e00;&#x4e2a;&#x6b21;&#x7f16;&#x53f7;"/>
+<node CREATED="1451032218269" ID="ID_83146685" MODIFIED="1451032338810" TEXT="name&#x662f;&#x94fe;&#x63a5;&#x5230;&#x8fd9;&#x4e2a;&#x8303;&#x56f4;&#x7684;&#x8bbe;&#x5907;&#x7684;&#x540d;&#x5b57;/proc/devices /proc/sysfs"/>
+</node>
+<node CREATED="1451032363453" FOLDED="true" ID="ID_751636723" MODIFIED="1451032600248" TEXT="int alloc_chrdev_region(dev_t *dev,unsigned int firstminor ,unsigned int count,char *name);">
+<node CREATED="1451032411648" ID="ID_168156763" MODIFIED="1451032434154" TEXT="dev&#x662f;&#x4e00;&#x4e2a;&#x53ea;&#x8f93;&#x51fa;&#x7684;&#x53c2;&#x6570;"/>
+<node CREATED="1451032434829" ID="ID_1706272048" MODIFIED="1451032465452" TEXT="firstminor&#x662f;&#x7b2c;&#x4e00;&#x4e2a;&#x6b21;&#x8bbe;&#x5907;&#x7f16;&#x53f7;"/>
+<node CREATED="1451032466285" ID="ID_1147324425" MODIFIED="1451032500890" TEXT="count&#x548c;name&#x540c;register_chrdev_region"/>
+</node>
+<node CREATED="1451032602285" ID="ID_1759977214" MODIFIED="1451032641838" TEXT="void unregister_chrdev_region(dev_t first , unsigned int count);"/>
+</node>
+<node CREATED="1451032864077" ID="ID_1685868388" MODIFIED="1451036503269" TEXT="&#x4e3b;&#x7f16;&#x53f7;&#x52a8;&#x6001;&#x5206;&#x914d;">
+<node CREATED="1451032878304" ID="ID_819676075" MODIFIED="1451033064587" TEXT="&#x5c3d;&#x91cf;&#x7528;alloc_chrdev_region&#x800c;&#x4e0d;&#x662f;register_chrdev_region"/>
+<node CREATED="1451036402289" ID="ID_979050161" MODIFIED="1451036420703" TEXT="/proc/devices&#x4e2d;&#x53ef;&#x4ee5;&#x8bfb;&#x53d6;&#x8bbe;&#x5907;&#x53f7;"/>
+<node CREATED="1451036504722" ID="ID_236969610" MODIFIED="1451036531203" TEXT="&#x83b7;&#x53d6;&#x4e3b;&#x7f16;&#x53f7;">
+<node CREATED="1451036531204" FOLDED="true" ID="ID_1816458452" MODIFIED="1451036673007" TEXT="scull_major != 0&#x9759;&#x6001;&#x5206;&#x914d;&#x4e3b;&#x8bbe;&#x5907;&#x53f7;">
+<node CREATED="1451036546900" ID="ID_1202371730" MODIFIED="1451036551440" TEXT="MKDEV"/>
+<node CREATED="1451036551938" ID="ID_211173919" MODIFIED="1451036565438" TEXT="register_chrdev_region"/>
+</node>
+<node CREATED="1451036567586" ID="ID_1156308804" MODIFIED="1451036688336" TEXT="scull_major == 0 &#x52a8;&#x6001;&#x5206;&#x914d;&#x4e3b;&#x8bbe;&#x5907;&#x53f7;">
+<node CREATED="1451036576420" ID="ID_1226264816" MODIFIED="1451036588719" TEXT="alloc_chrdev_region"/>
+<node CREATED="1451036589458" ID="ID_215282782" MODIFIED="1451036622756" TEXT="MAJOR(dev)"/>
+</node>
+</node>
+</node>
+<node CREATED="1451036693458" ID="ID_1298640210" MODIFIED="1451036700772" TEXT="&#x91cd;&#x8981;&#x6570;&#x636e;&#x7ed3;&#x6784;">
+<node CREATED="1451036700772" ID="ID_1456670018" MODIFIED="1451036855294" TEXT="file_operations fops">
+<node CREATED="1451037049842" ID="ID_934679588" MODIFIED="1451037060786" TEXT="struct module *owner"/>
+</node>
+</node>
+</node>
+<node CREATED="1451031228844" ID="ID_1041208982" MODIFIED="1451031228844" POSITION="right" TEXT=""/>
+<node CREATED="1451031147692" ID="ID_1227945138" MODIFIED="1451031147692" POSITION="right" TEXT=""/>
+</node>
+</map>
